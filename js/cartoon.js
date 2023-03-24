de8020be28844c36da2df89ac18ab3b8d4839ecd
huamingy <2191452146@qq.com>
2023-03-24 14:10:49 +0800

    动画播放

---
+++ js/cartoon.js
@@ -0,0 +1,41 @@
+/** @format */
+
+import * as THREE from '../build/three.module.js';
+import {FBXLoader} from '../three/jsm/loaders/FBXLoader.js';
+import {GUI} from '../three/jsm/libs/dat.gui.module.js';
+import {GLTFLoader} from '../three/jsm/loaders/GLTFLoader.js';
+import {renderer, scene, camera, controls} from './scene.js';
+
+var clock = new THREE.Clock();
+
+var mixer = null;
+
+// 加载光照贴图
+
+var loader = new GLTFLoader();
+var actions = [];
+loader.load('../models/fendShan_Cartoon/AirconditionBoxFanAnimation.glb', function (fbx) {
+  scene.add(fbx.scene);
+  console.log('fbx.scene: ', fbx);
+
+  mixer = new THREE.AnimationMixer(fbx.scene);
+  //所有的动画数组
+
+  for (var i = 0; i < 8; i++) {
+    actions[i] = mixer.clipAction(fbx.animations[i]);
+  }
+
+  actions[0].play();
+});
+
+function animate(time) {
+  var time = clock.getDelta();
+  if (mixer) {
+    mixer.update(time);
+  }
+  TWEEN.update();
+  // stats.update(); // 初始化stats后，需要在这里执行update方法才能实现fps实时监控
+  renderer.render(scene, camera); // 最后需要将场景渲染出来，没有这句将什么都显示不了
+  requestAnimationFrame(animate); // 这里利用浏览器API——requestAnimationFrame，每帧都进行渲染，执行renderer.render(...)方法
+}
+animate();
